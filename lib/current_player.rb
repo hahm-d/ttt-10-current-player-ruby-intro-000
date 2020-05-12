7c9c83f8fc3f7669285d1d28e1c0b392afaa4166
danny <ohdangitsdaniel@gmail.com>
2020-05-12 02:53:16 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,4 +6,11 @@ def turn_count(board)
     end
   end
   counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0 
+    return "X"
+  elsif turn_count(board) % 2 == 1 
+    return "O"
 end 
