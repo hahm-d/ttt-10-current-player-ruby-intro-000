528fa77aed7480bb5d1715570dc38c1b38233abe
danny <ohdangitsdaniel@gmail.com>
2020-05-12 02:50:33 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,9 @@
 def turn_count(board)
   counter  = 0;
   board.each do |player|
-  if player == "X" || player == "O"  
-    counter += 1;
+    if player == "X" || player == "O"  
+      counter += 1;
+    end
   end
   counter
 end 
